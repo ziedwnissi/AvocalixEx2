da3ff29c82d31fbd17b0329d192ca4163e9bd252
ziedwnissi <ziedwnissi@gmail.com>
2019-04-07 18:22:00 +0200

    fix bug

--- avocalixex2/src/factforms.js
+++ avocalixex2/src/factforms.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
   class FactForms extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      //this.handleChange = this.handleChange.bind(this);
+      //this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+
+
     render() {
       return (
         <div>
@@ -8,20 +23,20 @@ import React, { Component } from 'react';
         <div>
         <form action ="/action_page.php">
         <label>Libellé</label>
-        <input type="text"id="fname"name="firstname"placeholder="Your name.."/>
+        <input type="text"id="fname"name="firstname"placeholder="Libellé"/>
         
 
         <label>Email</label>
         <input type="email"id="email"name="email"placeholder="Your email"/>
 
         <label>Montant de la préstation</label>
-        <input type="text"id="fname"name="firstname"placeholder="Your name.."/>
+        <input type="text" id="montant" name="Montant"placeholder="Montant de la préstation" onChange={this.handleChange}/>
 
         <label>Valeur de TVA</label>
-        <input type="text"id="fname"name="firstname"placeholder="Your name.."/>
+        <input type="text"id="fname"name="firstname"placeholder="Valeur TVA" value={this.state.value*0.2}/>
         
         <label>Total</label>
-        <input type="text"id="fname"name="firstname"placeholder="Your name.."/>
+        <input type="text"id="fname"name="firstname"placeholder="Total"/>
         
         
         
